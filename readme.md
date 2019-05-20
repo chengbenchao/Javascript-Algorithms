aa764f7a83253ab556b2e753cf90aef490bbdf12
chengbenchao <395775347@qq.com>
2019-05-20 11:50:06 +0800

    leetcoe

---
+++ readme.md
@@ -0,0 +1,1 @@
+[leetcode](https://leetcode-cn.com/problemset/all/)
---
+++ test/sum.test.js
@@ -0,0 +1,5 @@
+import sum from '../code/string/reverse'
+test('reverse world', () => {
+  // eslint-disable-next-line jest/valid-expect
+  expect(sum("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
+})
